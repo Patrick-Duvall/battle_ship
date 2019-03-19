072518a807b8b7bd93de9ba7e919e0f7f2da96b8
Patrick Duvall <duvallp208@gmail.com>
2019-03-18 18:55:46 -0600

    Add cell tests
    Co-authored-by: Chris <davisc1014@gmail.com>

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -12,5 +12,32 @@ describe Cell do
     expect(@cell).to be_a(Cell)
   end
 
-  
+  it "has coordinate" do
+    expect(@cell.coordinate).to eq("B4")
+  end
+
+  it "does not have a ship by default" do
+    expect(@cell.ship).to eq(nil)
+    expect(@cell.empty?).to eq(true)
+  end
+  it "has a ship when placed" do
+    expect(@cell.ship).to eq(@ship)
+    expect(@cell.empty?).to eq(false)
+  end
+
+  it "may be fired upon" do
+    expect(@cell.fired_upon?).to eq(false)
+    @cell.fire_upon
+    expect(@cell.fired_upon?).to eq(true)
+  end
+  it "being fired_upon reduces ship health" do
+    @cell.place_ship(@ship)
+    expect(@cell.ship.health).to eq(3)
+    @cell.fire_upon
+    expect(@cell.ship.health).to eq(2)
+    @cell.fire_upon
+    expect(@cell.ship.health).to eq(1)
+  end
+
+
 end
