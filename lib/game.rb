de1993f265d7af5131f0bffab144d46099489d92
davisc0801 <davisc1014@gmail.com>
2019-03-21 11:41:19 -0600

    Update notes for UI

--- lib/game.rb
+++ lib/game.rb
@@ -17,13 +17,6 @@ class Game
     until answer.to_i #boardsize
       answer = gets.chomp
     end
-    #baord size - min 4, max 10
-    #ship stats?? ship number boardsize - 1
-    #ship length boardsize - 1
-    #cpu plays with same ships
-    # if player says standard
-    #
-    # auto-size ship based on coordinates given?
   end
 
   # def standard_fleet
