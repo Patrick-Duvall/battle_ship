cf291906d4dbf615087e41f7d341f4275bc5b12e
davisc0801 <davisc1014@gmail.com>
2019-03-21 15:01:27 -0600

    Flesh out prompts and conditionals for board setup
    Co-authored-by: Patrick <duvallp208@gmail.com>

--- lib/game.rb
+++ lib/game.rb
@@ -1,30 +1,111 @@
+require "./lib/ship"
+require "./lib/cell"
+require "./lib/board"
+
 class Game
 
-  # def initialize
-    # cpu_place_ship
-  # end
+  def initialize
+    @ships = []
+  end
 
   # user interaction tests
 
   def welcome
-    p "Welcome to BATTLESHIP \n Enter p to play. Enter q to quit."
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+    answered = false
+    while answered == false
+      print "> "
+      input = gets.chomp
+      case
+        when input.downcase == "p" || input.downcase == "play"
+          print "\n"
+          answered = true
+        when input.downcase == "q" || input.downcase == "quit"
+          puts "Thanks for playing!"
+          exit
+      end
+    end
+  end
+
+  def choose_game
+    puts "Would you like to play a small game, a full game, or a custom game?"
+    puts "Enter either small, full, or custom."
+    answered = false
+    while answered == false
+      print "> "
+      input = gets.chomp
+      case
+        when input.downcase == "c" || input.downcase == "custom"
+          self.set_board_size
+          answered = true
+        when input.downcase == "f" || input.downcase == "full"
+          self.make_full_game
+          answered = true
+        when input.downcase == "s" || input.downcase == "standard"
+          self.make_small_game
+          answered = true
+      end
+    end
+  end
+
+  def make_full_game
+    @playerboard = Board.new
+    @cpuboard = Board.new
+    @playerboard.cell_gen(10)
+    @cpuboard.cell_gen(10)
+    destroyer = Ship.new("Destroyer", 2)
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 3)
+    battleship = Ship.new("battleship", 4)
+    carrier = Ship.new("Carrier", 5)
+    @ships = [destroyer, cruiser, submarine, battleship, carrier]
+  end
+
+  def make_small_game
+    @playerboard = Board.new
+    @cpuboard = Board.new
+    @playerboard.cell_gen(4)
+    @cpuboard.cell_gen(4)
+    submarine = Ship.new("Submarine", 2)
+    cruiser = Ship.new("Cruiser", 3)
+    @ships = [cruiser, submarine]
+  end
+
+  def set_board_size
+    answered = false
+    while answered == false
+      puts "Please select the size of the board."
+      puts "Enter only a single number, between 4 and 10, for the size."
+      print "> "
+      input = gets.chomp
+      answered = (4..10).to_a.include?(input.to_i)
+    end
+    print "\n"
+    @playerboard = Board.new
+    @cpuboard = Board.new
+    @playerboard.cell_gen(input.to_i)
+    @cpuboard.cell_gen(input.to_i)
+    self.make_custom_fleet
+  end
+
+  def make_custom_fleet
+    puts "How many ships would you like to play with?"
   end
 
   def place_ship_prompt
-    p "I have laid out my ships on the grid."
-    p "You now need to lay out your ships."
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your ships."
     answer = nil
     until answer.to_i #boardsize
       answer = gets.chomp
     end
   end
-
-  # def standard_fleet
-  #   @subplayer = Ship.new("Submarine", 2)
-  #   @cruiserplayer = Ship.new("Cruiser", 3)
-  # end
-
-  def cpu_place_ship
-
-  end
 end
+
+# 
+# g = Game.new
+#
+# g.welcome
+# g.set_board_size
+# g.choose_game
