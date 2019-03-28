bfa9f92b4fb5127e25bfbc3ae1f1703028727604
davisc0801 <davisc1014@gmail.com>
2019-03-27 18:09:48 -0600

    Complete AI weighted to dynamic most probable locations

--- lib/cpu.rb
+++ lib/cpu.rb
@@ -5,6 +5,7 @@ def initialize(cpuboard, playerboard)
   @playerboard = playerboard
   @lasthit = nil
   @lastshotwashit = false
+  @targetships = []
 end
 
 ## Can build array with Nils BUT determine_cpu_placement rejects them
@@ -15,13 +16,14 @@ end
     when 1
       square = first_square[0] + ((first_square[1].to_i) - 1).to_s
     when 2
-      square = ((first_square[0].ord) + 1).chr + first_square[1]
+      square = ((first_square[0].ord) + 1).to_s + first_square[1]
     when 3
-      square = ((first_square[0].ord) - 1).chr + first_square[1]
+      square = ((first_square[0].ord) - 1).to_s + first_square[1]
     end
   end
 
   def determine_cpu_placement(ship_array)
+    @targetships = ship_array
     i = 0
     until i == ship_array.length do
       first_square = @cpuboard.cells.keys.sample
@@ -37,12 +39,64 @@ end
     end
   end
 
+  def distance_to_edge(cell)
+    count = 0
+    @targetships.each do |ship|
+      findshiphealth = ship.health
+      ncelledge = nil
+      scelledge = nil
+      ecelledge = nil
+      wcelledge = nil
+      ncounter = 1
+      scounter = 1
+      ecounter = 1
+      wcounter = 1
+      until ncelledge != nil
+        if @playerboard.valid_coordinate?((((cell[0]).ord - ncounter).chr + cell[1]))
+          ncounter += 1
+        else
+          count += 1 if findshiphealth <= ncounter
+          ncelledge = "done"
+        end
+      end
+      until scelledge != nil
+        if @playerboard.valid_coordinate?((((cell[0]).ord + scounter).chr + cell[1]))
+          scounter += 1
+        else
+          count += 1 if findshiphealth <= scounter
+          scelledge = "done"
+        end
+      end
+      until ecelledge != nil
+        if @playerboard.valid_coordinate?(cell[0] + (((cell[1]).to_i - ecounter).to_s))
+          ecounter += 1
+        else
+          count += 1 if findshiphealth <= ecounter
+          ecelledge = "done"
+        end
+      end
+      until wcelledge != nil
+        if @playerboard.valid_coordinate?(cell[0] + (((cell[1]).to_i + wcounter).to_s))
+          wcounter += 1
+        else
+          count += 1 if findshiphealth <= wcounter
+          wcelledge = "done"
+        end
+      end
+    end
+    return count
+  end
+
   def cpu_shot
     chosen = false
     coordinate = nil
+    weight = Hash.new(0)
+    weight.merge!(@cpuboard.cells)
     until chosen == true
-      coordinate = @playerboard.cells.keys.sample
+      weight.each_key{|key| weight[key] = distance_to_edge(key)} if !@lastshotwashit
+      coordinate = weight.max_by{|key, value| value}[0]
       chosen = true if @playerboard.valid_coordinate?(coordinate)
+      weight.delete(coordinate)
     end
     if @lastshotwashit == true && @lasthit != nil
       chosen = false
@@ -77,6 +131,4 @@ end
         @lastshotwashit = false
     end
   end
-
-
 end
